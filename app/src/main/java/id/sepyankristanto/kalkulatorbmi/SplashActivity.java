69847fda73774f4ec6796a6ea26c88592e398274
Sepyan Kristanto <sepyankristanto@Sepyans-MacBook-Pro.local>
2024-07-16 11:05:53 +0700

    splah check

--- app/src/main/java/id/sepyankristanto/kalkulatorbmi/SplashActivity.java
+++ app/src/main/java/id/sepyankristanto/kalkulatorbmi/SplashActivity.java
@@ -7,15 +7,20 @@ import android.os.Handler;
 
 public class SplashActivity extends Activity {
     Handler handler;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashbmi);
-        handler = new Handler();
-        handler.postDelayed(() -> {
-            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        }, 3000);
-    }
 
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
 }
